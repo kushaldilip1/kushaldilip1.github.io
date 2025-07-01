f31c62c455de2099570d28a884fefb12f95afb7c
kushaldilip1 <kushaldilip18@gmail.com>
2025-07-01 19:22:28 +0100

    Static image import

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,31 +1,32 @@
 "use client";
 
 import { motion } from "framer-motion";
-import Image from "next/image"; 
+import Image from "next/image";
+import portrait from '@/public/assets/portrait_correct_one.png';
 
 const Photo = () => {
     return (
         <div className="w-full h-full relative">
-            <motion.div 
-            initial= {{ opacity: 0}}
-            animate = {{
-                opacity: 1,
-                transition: {delay: 1, duration: 0.4, ease: 'easeIn'},
-            }}
-            
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{
+                    opacity: 1,
+                    transition: { delay: 1, duration: 0.4, ease: 'easeIn' },
+                }}
+
             >
                 {/*     IMAGE     */}
                 <motion.div
-                initial= {{ opacity: 0}}
-                animate = {{
-                    opacity: 1,
-                    transition: {delay: 0.5, duration: 0.4, ease: 'easeInOut'},
-                }} 
-                className="w-[228px] h-[228px] md:w-[358px] md:h-[358px] ml-20 mb-4">
+                    initial={{ opacity: 0 }}
+                    animate={{
+                        opacity: 1,
+                        transition: { delay: 0.5, duration: 0.4, ease: 'easeInOut' },
+                    }}
+                    className="w-[228px] h-[228px] md:w-[358px] md:h-[358px] ml-20 mb-4">
                     <Image
-                    src="/assets/portrait_correct_one.png"
-                    priority quality={100}
-                    fill alt="" className="object-contain"
+                        src={portrait}
+                        priority quality={100}
+                        fill alt="" className="object-contain"
                     />
                 </motion.div>
 
